62856f1055cf434667c0af76f6908817f6ca8b43
Chekushka <chekun.sergiy@gmail.com>
2020-07-17 12:55:40 +0300

    Initializing project.

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown(KeyCode.Escape.ToString()))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Resume();
+        SceneManager.LoadScene("Main Menu");
+        Debug.Log("Loading menu...");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting...");
+        Application.Quit();
+    }
+}
